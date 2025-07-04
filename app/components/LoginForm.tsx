b409fc79f7750f3dbc0ccb50344009779068d613
abraham241 <huguesmboundou@gmail.com>
2025-07-04 17:08:51 +0100

    fin du formulaire

--- app/components/LoginForm.tsx
+++ app/components/LoginForm.tsx
@@ -1,15 +1,21 @@
 "use client";
 import React from "react";
 import "./login.css";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function LoginForm() {
   return (
     <div className="login-page">
       <div className="login-card">
-        <div className="login-header">
-          <h2>Login to your account</h2>
-          <p>Entrez votre email ci-dessous pour vous connecter à votre compte</p>
-          <a href="#" className="signup-link">Sign Up</a>
+        <div className="flex flex-col justify-center items-center  login-header">
+          <Image
+            src="/logo.png"
+            alt="Picture of the author"
+            width={150}
+            height={150}
+          />
+          <p className="pt-2 text-2xl text-blue-500">Connectez-vous</p>
         </div>
         <form className="login-form">
           <label>Email</label>
@@ -17,11 +23,16 @@ export default function LoginForm() {
 
           <div className="password-field">
             <label>Mot de passe</label>
-            <a href="#" className="forgot-link">Forgot your password?</a>
+            <a href="#" className="forgot-link">
+              Mot de passe oublier
+            </a>
           </div>
           <input type="password" required />
-
-          <button type="submit" className="btn black">Login</button>
+          <Link href="/dashboard">
+            <button type="submit" className="btn black w-full">
+              Connexion
+            </button>
+          </Link>
         </form>
       </div>
     </div>
